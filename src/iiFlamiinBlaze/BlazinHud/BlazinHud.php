b7154ea7413ca947868219d46039fa87d5f9bdec
iiFlamiinBlaze <iiflamiinblaze123@gmail.com>
2018-06-18 17:37:39 -0400

    Bump to API 3.0.0

--- src/iiFlamiinBlaze/BlazinHud/BlazinHud.php
+++ src/iiFlamiinBlaze/BlazinHud/BlazinHud.php
@@ -33,158 +33,158 @@ use pocketmine\utils\TextFormat;
 
 class BlazinHud extends PluginBase implements Listener{
 
-    const VERSION = "v1.0.3";
-    const PREFIX = TextFormat::AQUA . "BlazinHud" . TextFormat::GOLD . " > ";
+	const VERSION = "v1.0.4";
+	const PREFIX = TextFormat::AQUA . "BlazinHud" . TextFormat::GOLD . " > ";
 
-    /** @var self $instance */
-    private static $instance;
-    /** @var array $hud */
-    public $hud = [];
+	/** @var self $instance */
+	private static $instance;
+	/** @var array $hud */
+	public $hud = [];
 
-    public function onEnable() : void{
-        self::$instance = $this;
-        @mkdir($this->getDataFolder());
-        $this->saveDefaultConfig();
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->economyCheck();
-        $this->getLogger()->info("BlazinHud " . self::VERSION . " by iiFlamiinBlaze is enabled");
-    }
+	public function onEnable() : void{
+		self::$instance = $this;
+		@mkdir($this->getDataFolder());
+		$this->saveDefaultConfig();
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->economyCheck();
+		$this->getLogger()->info("BlazinHud " . self::VERSION . " by iiFlamiinBlaze is enabled");
+	}
 
-    public function onJoin(PlayerJoinEvent $event) : void{
-        $this->multiWorldCheck($event->getPlayer());
-        $this->hud[] = $event->getPlayer()->getName();
-    }
+	public function onJoin(PlayerJoinEvent $event) : void{
+		$this->multiWorldCheck($event->getPlayer());
+		$this->hud[] = $event->getPlayer()->getName();
+	}
 
-    public function onLevelChange(EntityLevelChangeEvent $event) : void{
-        $this->multiWorldCheck($event->getEntity());
-    }
+	public function onLevelChange(EntityLevelChangeEvent $event) : void{
+		$this->multiWorldCheck($event->getEntity());
+	}
 
-    private function multiWorldCheck(Entity $entity) : bool{
-        if(!$entity instanceof Player) return false;
-        if($this->getConfig()->get("multi-world") === "on"){
-            if(in_array($entity->getLevel()->getName(), $this->getConfig()->get("worlds"))){
-                $this->getServer()->getScheduler()->scheduleRepeatingTask(new HudTask($this), 30);
-            }else{
-                $entity->sendMessage(self::PREFIX . TextFormat::RED . "You are not in the right world for your hud to appear");
-                return false;
-            }
-        }elseif($this->getConfig()->get("multi-world") === "off"){
-            $this->getServer()->getScheduler()->scheduleRepeatingTask(new HudTask($this), 30);
-            return false;
-        }
-        return true;
-    }
+	private function multiWorldCheck(Entity $entity) : bool{
+		if(!$entity instanceof Player) return false;
+		if($this->getConfig()->get("multi-world") === "on"){
+			if(in_array($entity->getLevel()->getName(), $this->getConfig()->get("worlds"))){
+				$this->getScheduler()->scheduleRepeatingTask(new HudTask(), 30);
+			}else{
+				$entity->sendMessage(self::PREFIX . TextFormat::RED . "You are not in the right world for your hud to appear");
+				return false;
+			}
+		}elseif($this->getConfig()->get("multi-world") === "off"){
+			$this->getScheduler()->scheduleRepeatingTask(new HudTask(), 30);
+			return false;
+		}
+		return true;
+	}
 
-    private function economyCheck() : bool{
-        if($this->getConfig()->get("economy") === "on"){
-            if($this->getServer()->getPluginManager()->getPlugin("EconomyAPI") === null){
-                $this->getPluginLoader()->disablePlugin($this);
-                $this->getLogger()->error(TextFormat::RED . "Plugin Disabled! Please turn off economy support in the config or enable/install EconomyAPI");
-                return false;
-            }
-        }elseif($this->getConfig()->get("economy") === "off") return false;
-        return true;
-    }
+	private function economyCheck() : bool{
+		if($this->getConfig()->get("economy") === "on"){
+			if($this->getServer()->getPluginManager()->getPlugin("EconomyAPI") === null){
+				$this->getServer()->getPluginManager()->disablePlugin($this);
+				$this->getLogger()->error(TextFormat::RED . "Plugin Disabled! Please turn off economy support in the config or enable/install EconomyAPI");
+				return false;
+			}
+		}elseif($this->getConfig()->get("economy") === "off") return false;
+		return true;
+	}
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        if($command->getName() === "blazinhud"){
-            if(!$sender instanceof Player){
-                $sender->sendMessage(self::PREFIX . TextFormat::RED . "Use this command in game");
-                return false;
-            }
-            if(!$sender->hasPermission("blazinhud.command")){
-                $sender->sendMessage(self::PREFIX . TextFormat::RED . "You do not have permission to use this command");
-                return false;
-            }
-            if(empty($args)){
-                $sender->sendMessage(self::PREFIX . TextFormat::GRAY . "Usage: /blazinhud <info | set | reload | on | off> <hud | multiworld> <message>");
-                return false;
-            }
-            switch($args[0]){
-                case "info":
-                    foreach([TextFormat::DARK_GRAY . "-=========" . TextFormat::GOLD . "BlazinHud " . self::VERSION . TextFormat::DARK_GRAY . "=========-",
-                                TextFormat::GREEN . "Author: " . TextFormat::AQUA . "BlazeTheDev",
-                                TextFormat::GREEN . "GitHub: " . TextFormat::AQUA . "https://github.com/iiFlamiinBlaze",
-                                TextFormat::GREEN . "Support: " . TextFormat::AQUA . "https://discord.gg/znEsFsG",
-                                TextFormat::GREEN . "Description: " . TextFormat::AQUA . "Allows you to customize a message that will pop up above your hotbar",
-                                TextFormat::DARK_GRAY . "-===============================-"] as $msg) $sender->sendMessage($msg);
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		if($command->getName() === "blazinhud"){
+			if(!$sender instanceof Player){
+				$sender->sendMessage(self::PREFIX . TextFormat::RED . "Use this command in game");
+				return false;
+			}
+			if(!$sender->hasPermission("blazinhud.command")){
+				$sender->sendMessage(self::PREFIX . TextFormat::RED . "You do not have permission to use this command");
+				return false;
+			}
+			if(empty($args)){
+				$sender->sendMessage(self::PREFIX . TextFormat::GRAY . "Usage: /blazinhud <info | set | reload | on | off> <hud | multiworld> <message>");
+				return false;
+			}
+			switch($args[0]){
+				case "info":
+					foreach([TextFormat::DARK_GRAY . "-=========" . TextFormat::GOLD . "BlazinHud " . self::VERSION . TextFormat::DARK_GRAY . "=========-",
+								TextFormat::GREEN . "Author: " . TextFormat::AQUA . "BlazeTheDev",
+								TextFormat::GREEN . "GitHub: " . TextFormat::AQUA . "https://github.com/iiFlamiinBlaze",
+								TextFormat::GREEN . "Support: " . TextFormat::AQUA . "https://discord.gg/znEsFsG",
+								TextFormat::GREEN . "Description: " . TextFormat::AQUA . "Allows you to customize a message that will pop up above your hotbar",
+								TextFormat::DARK_GRAY . "-===============================-"] as $msg) $sender->sendMessage($msg);
 
-                    return true;
-                case "on":
-                    if(!in_array($sender->getName(), $this->hud)){
-                        $this->hud[] = $sender->getName();
-                        $sender->sendMessage(self::PREFIX . TextFormat::GREEN . "You have turned on your hud");
-                    }elseif(in_array($sender->getName(), $this->hud)){
-                        $sender->sendMessage(self::PREFIX . TextFormat::RED . "Your hud is already on");
-                        return false;
-                    }
-                    return true;
-                case "off":
-                    if(in_array($sender->getName(), $this->hud)){
-                        unset($this->hud[array_search($sender->getName(), $this->hud)]);
-                        $sender->sendMessage(self::PREFIX . TextFormat::RED . "You have turned off your hud");
-                    }elseif(!in_array($sender->getName(), $this->hud)){
-                        $sender->sendMessage(self::PREFIX . TextFormat::RED . "Your hud is already off");
-                        return false;
-                    }
-                    return true;
-                case "set":
-                    if($args[1]){
-                        switch($args[1]){
-                            case "hud":
-                                if($args[2]){
-                                    if(is_string($args[2])){
-                                        $this->getConfig()->set("hud-message", $args[2]);
-                                        $this->getConfig()->save();
-                                        $sender->sendMessage(self::PREFIX . TextFormat::GREEN . "You have successfully set the hud to $args[2]");
-                                        return true;
-                                    }else{
-                                        $sender->sendMessage(self::PREFIX . TextFormat::RED . "Please enter a string to set the hud too");
-                                        return false;
-                                    }
-                                }else{
-                                    $sender->sendMessage(self::PREFIX . TextFormat::GRAY . "Usage: /blazinhud <info | set | reload | on | off> <hud | multiworld> <message>");
-                                    return false;
-                                }
-                            case "multiworld":
-                                if($args[2]){
-                                    switch($args[2]){
-                                        case "on":
-                                            $this->getConfig()->set("multiworld", "on");
-                                            $sender->sendMessage(self::PREFIX . TextFormat::GREEN . "You have successfully turned multiworld on");
-                                            return true;
-                                        case "off":
-                                            $this->getConfig()->set("multiworld", "off");
-                                            $sender->sendMessage(self::PREFIX . TextFormat::GREEN . "You have successfully turned multiworld off");
-                                            return true;
-                                        default:
-                                            $sender->sendMessage(self::PREFIX . TextFormat::GRAY . "Usage: " . TextFormat::GREEN . "/blazinhud multiworld " . TextFormat::RED . "on | off");
-                                            return false;
-                                    }
-                                }
-                                return false;
-                            default:
-                                $sender->sendMessage(self::PREFIX . TextFormat::GRAY . "Usage: /blazinhud <info | set | reload | on | off> <hud | multiworld> <message>");
-                                return false;
-                        }
-                    }else{
-                        $sender->sendMessage(self::PREFIX . TextFormat::GRAY . "Usage: /blazinhud <info | set | reload | on | off> <hud | multiworld> <message>");
-                        return false;
-                    }
-                case "reload":
-                    $this->getConfig()->save();
-                    $this->getConfig()->reload();
-                    $sender->sendMessage(self::PREFIX . TextFormat::GREEN . "BlazinHud successfully reloaded");
-                    return true;
-                default:
-                    $sender->sendMessage(self::PREFIX . TextFormat::GRAY . "Usage: /blazinhud <info | set | reload | on | off> <hud | multiworld> <message>");
-                    return true;
-            }
-        }
-        return true;
-    }
+					return true;
+				case "on":
+					if(!in_array($sender->getName(), $this->hud)){
+						$this->hud[] = $sender->getName();
+						$sender->sendMessage(self::PREFIX . TextFormat::GREEN . "You have turned on your hud");
+					}elseif(in_array($sender->getName(), $this->hud)){
+						$sender->sendMessage(self::PREFIX . TextFormat::RED . "Your hud is already on");
+						return false;
+					}
+					return true;
+				case "off":
+					if(in_array($sender->getName(), $this->hud)){
+						unset($this->hud[array_search($sender->getName(), $this->hud)]);
+						$sender->sendMessage(self::PREFIX . TextFormat::RED . "You have turned off your hud");
+					}elseif(!in_array($sender->getName(), $this->hud)){
+						$sender->sendMessage(self::PREFIX . TextFormat::RED . "Your hud is already off");
+						return false;
+					}
+					return true;
+				case "set":
+					if($args[1]){
+						switch($args[1]){
+							case "hud":
+								if($args[2]){
+									if(is_string($args[2])){
+										$this->getConfig()->set("hud-message", $args[2]);
+										$this->getConfig()->save();
+										$sender->sendMessage(self::PREFIX . TextFormat::GREEN . "You have successfully set the hud to $args[2]");
+										return true;
+									}else{
+										$sender->sendMessage(self::PREFIX . TextFormat::RED . "Please enter a string to set the hud too");
+										return false;
+									}
+								}else{
+									$sender->sendMessage(self::PREFIX . TextFormat::GRAY . "Usage: /blazinhud <info | set | reload | on | off> <hud | multiworld> <message>");
+									return false;
+								}
+							case "multiworld":
+								if($args[2]){
+									switch($args[2]){
+										case "on":
+											$this->getConfig()->set("multiworld", "on");
+											$sender->sendMessage(self::PREFIX . TextFormat::GREEN . "You have successfully turned multiworld on");
+											return true;
+										case "off":
+											$this->getConfig()->set("multiworld", "off");
+											$sender->sendMessage(self::PREFIX . TextFormat::GREEN . "You have successfully turned multiworld off");
+											return true;
+										default:
+											$sender->sendMessage(self::PREFIX . TextFormat::GRAY . "Usage: " . TextFormat::GREEN . "/blazinhud multiworld " . TextFormat::RED . "on | off");
+											return false;
+									}
+								}
+								return false;
+							default:
+								$sender->sendMessage(self::PREFIX . TextFormat::GRAY . "Usage: /blazinhud <info | set | reload | on | off> <hud | multiworld> <message>");
+								return false;
+						}
+					}else{
+						$sender->sendMessage(self::PREFIX . TextFormat::GRAY . "Usage: /blazinhud <info | set | reload | on | off> <hud | multiworld> <message>");
+						return false;
+					}
+				case "reload":
+					$this->getConfig()->save();
+					$this->getConfig()->reload();
+					$sender->sendMessage(self::PREFIX . TextFormat::GREEN . "BlazinHud successfully reloaded");
+					return true;
+				default:
+					$sender->sendMessage(self::PREFIX . TextFormat::GRAY . "Usage: /blazinhud <info | set | reload | on | off> <hud | multiworld> <message>");
+					return true;
+			}
+		}
+		return true;
+	}
 
-    public static function getInstance() : self{
-        return self::$instance;
-    }
+	public static function getInstance() : self{
+		return self::$instance;
+	}
 }
--- src/iiFlamiinBlaze/BlazinHud/HudTask.php
+++ src/iiFlamiinBlaze/BlazinHud/HudTask.php
@@ -21,44 +21,40 @@ declare(strict_types=1);
 
 namespace iiFlamiinBlaze\BlazinHud;
 
-use pocketmine\scheduler\PluginTask;
+use pocketmine\scheduler\Task;
 
-class HudTask extends PluginTask{
+class HudTask extends Task{
 
-    public function __construct(BlazinHud $main){
-        parent::__construct($main);
-    }
-
-    public function onRun(int $tick) : void{
-        $hud = BlazinHud::getInstance()->getConfig()->get("hud-message");
-        foreach(BlazinHud::getInstance()->getServer()->getOnlinePlayers() as $player){
-            if(!in_array($player->getName(), BlazinHud::getInstance()->hud)) return;
-            $hud = str_replace([
-                "{line}",
-                "{max_players}",
-                "{online_players}",
-                "&",
-                "{x}",
-                "{y}",
-                "{z}",
-                "{level}",
-                "{tps}",
-                "{motd}",
-                "{money}"
-            ], [
-                "\n",
-                BlazinHud::getInstance()->getServer()->getMaxPlayers(),
-                count(BlazinHud::getInstance()->getServer()->getOnlinePlayers()),
-                "§",
-                (string)round($player->getX()),
-                (string)round($player->getY()),
-                (string)round($player->getZ()),
-                $player->getLevel()->getName(),
-                BlazinHud::getInstance()->getServer()->getTicksPerSecond(),
-                BlazinHud::getInstance()->getServer()->getMotd(),
-                BlazinHud::getInstance()->getServer()->getPluginManager()->getPlugin("EconomyAPI")->getInstance()->myMoney($player)
-            ], $hud);
-            $player->sendPopup($hud);
-        }
-    }
+	public function onRun(int $tick) : void{
+		$hud = BlazinHud::getInstance()->getConfig()->get("hud-message");
+		foreach(BlazinHud::getInstance()->getServer()->getOnlinePlayers() as $player){
+			if(!in_array($player->getName(), BlazinHud::getInstance()->hud)) return;
+			$hud = str_replace([
+				"{line}",
+				"{max_players}",
+				"{online_players}",
+				"&",
+				"{x}",
+				"{y}",
+				"{z}",
+				"{level}",
+				"{tps}",
+				"{motd}",
+				"{money}"
+			], [
+				"\n",
+				BlazinHud::getInstance()->getServer()->getMaxPlayers(),
+				count(BlazinHud::getInstance()->getServer()->getOnlinePlayers()),
+				"§",
+				(string)round($player->getX()),
+				(string)round($player->getY()),
+				(string)round($player->getZ()),
+				$player->getLevel()->getName(),
+				BlazinHud::getInstance()->getServer()->getTicksPerSecond(),
+				BlazinHud::getInstance()->getServer()->getMotd(),
+				BlazinHud::getInstance()->getServer()->getPluginManager()->getPlugin("EconomyAPI")->getInstance()->myMoney($player)
+			], $hud);
+			$player->sendPopup($hud);
+		}
+	}
 }
